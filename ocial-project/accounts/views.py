ca1b84f2c0687170dba21279c2a109e53cfcb550
Onur Asıliskender <onurasiliskender@gmail.com>
2019-04-05 22:59:21 +0300

    User Login

---
+++ ocial-project/accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+def signup(request):
+	if request.method == 'POST':
+		if request.POST['password'] == request.POST['password2']:
+			try:
+				user = User.objects.get(username = request.POST['username'])
+				return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+			except User.DoesNotExist:
+				user = User.objects.create_user(request.POST['username'], password = request.POST['password'])
+				auth.login(request,user)
+				return redirect('home') 
+		else:
+			return render(request, 'accounts/signup.html', {'error': 'Password must match'})
+
+	else:
+		return render(request, 'accounts/signup.html')
+
+
+def login(request):
+	return render(request, 'accounts/login.html')
+
+def logout(request):
+	#TODO need to route to home page
+	return render(request, 'accounts/login.html')
