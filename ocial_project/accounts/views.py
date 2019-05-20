2aa3490a4ac63249c15a58124fb05c808173fe92
Onur Asıliskender <onurasiliskender@gmail.com>
2019-05-20 12:12:57 +0300

    signup page bug correction

--- ocial_project/accounts/views.py
+++ ocial_project/accounts/views.py
@@ -13,19 +13,24 @@ from .models import *
 
 def signup(request):
 	if request.method == 'POST':
-		if request.POST['password'] == request.POST['password2']:
-			try:
-				user = User.objects.get(username = request.POST['username'])
-				return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
-
-				#TODO email must be checked.
-			except User.DoesNotExist:
-				user = User.objects.create_user(request.POST['username'], password = request.POST['password'], email = request.POST['email'],first_name = request.POST['firstname'],last_name = request.POST['lastname'])
-				auth.login(request,user)
-				return redirect('home') 
+		if request.POST['password'] and request.POST['username'] and request.POST['email']:
+			if request.POST['password'] == request.POST['password2']:
+				try:
+					user = User.objects.get(username = request.POST['username'])
+					return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+				except:
+					try:
+						user = User.objects.get(email = request.POST['email'])
+						return render(request, 'accounts/signup.html', {'error': 'E-Mail is used by other user.'})
+					except User.DoesNotExist:
+						user = User.objects.create_user(request.POST['username'], password = request.POST['password'], email = request.POST['email'],first_name = request.POST['firstname'],last_name = request.POST['lastname'])
+						auth.login(request,user)
+						return redirect('home') 
+			else:
+				return render(request, 'accounts/signup.html', {'error': 'Password must match'})
 		else:
-			return render(request, 'accounts/signup.html', {'error': 'Password must match'})
-
+			print(request.POST)
+			return render(request, 'accounts/signup.html', {'error': 'Username, E-Mail or Password cannot be empty.'})
 	else:
 		return render(request, 'accounts/signup.html')
 
